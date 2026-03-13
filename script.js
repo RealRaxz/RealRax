3b767390d9d8fe1c15e772ee057149495b19f413
RealRaxz <xxzzoo500@gmail.com>
2026-03-13 20:52:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,111 +1,69 @@
-const scripts=[
+const scripts = [
 
-{
-title:"Auto Farm Script",
-img:"https://picsum.photos/400/250?1",
-link:"script1.html"
-},
+{title:"Auto Farm Script",img:"https://picsum.photos/400/250?1",link:"script1.html"},
+{title:"Fast Attack Script",img:"https://picsum.photos/400/250?2",link:"script2.html"},
+{title:"Auto Raid Script",img:"https://picsum.photos/400/250?3",link:"script3.html"},
+{title:"Auto Boss Script",img:"https://picsum.photos/400/250?4",link:"script4.html"},
+{title:"Auto Mastery Script",img:"https://picsum.photos/400/250?5",link:"script5.html"},
+{title:"Sea Event Script",img:"https://picsum.photos/400/250?6",link:"script6.html"},
+{title:"Auto Level Script",img:"https://picsum.photos/400/250?7",link:"script1.html"},
+{title:"Auto Quest Script",img:"https://picsum.photos/400/250?8",link:"script1.html"},
+{title:"Fruit Finder Script",img:"https://picsum.photos/400/250?9",link:"script1.html"}
 
-{
-title:"Fast Attack Script",
-img:"https://picsum.photos/400/250?2",
-link:"script2.html"
-},
+];
 
-{
-title:"Auto Raid Script",
-img:"https://picsum.photos/400/250?3",
-link:"script3.html"
-},
+let page = 1;
+const perPage = 6;
 
-{
-title:"Auto Boss Script",
-img:"https://picsum.photos/400/250?4",
-link:"script4.html"
-},
+function renderPage(){
 
-{
-title:"Auto Mastery Script",
-img:"https://picsum.photos/400/250?5",
-link:"script5.html"
-},
+const grid = document.getElementById("scriptGrid");
+grid.innerHTML="";
 
-{
-title:"Auto Sea Event",
-img:"https://picsum.photos/400/250?6",
-link:"script6.html"
-},
+let start = (page-1)*perPage;
+let end = start+perPage;
 
-{
-title:"Auto Level Script",
-img:"https://picsum.photos/400/250?7",
-link:"script1.html"
-},
+let items = scripts.slice(start,end);
 
-{
-title:"Auto Quest Script",
-img:"https://picsum.photos/400/250?8",
-link:"script1.html"
-},
+items.forEach(function(s){
 
-{
-title:"Auto Fruit Script",
-img:"https://picsum.photos/400/250?9",
-link:"script1.html"
-}
+let card = document.createElement("a");
+card.className="card";
+card.href=s.link;
 
-]
-
-let page=1
-const perPage=6
-
-function render(){
-
-const grid=document.getElementById("scriptGrid")
-grid.innerHTML=""
-
-let start=(page-1)*perPage
-let end=start+perPage
-
-let items=scripts.slice(start,end)
-
-items.forEach(s=>{
-
-grid.innerHTML+=`
-
-<a class="card" href="${s.link}">
+card.innerHTML = `
 <img src="${s.img}">
 <h3>${s.title}</h3>
-</a>
+`;
 
-`
+grid.appendChild(card);
 
-})
+});
 
-document.getElementById("pageNum").innerText=page
-
-}
-
-function nextPage(){
-
-if(page*perPage<scripts.length){
-
-page++
-render()
+document.getElementById("pageNum").innerText = page;
 
 }
 
+document.getElementById("nextBtn").onclick=function(){
+
+if(page*perPage < scripts.length){
+
+page++;
+renderPage();
+
 }
 
-function prevPage(){
+}
+
+document.getElementById("prevBtn").onclick=function(){
 
 if(page>1){
 
-page--
-render()
+page--;
+renderPage();
 
 }
 
 }
 
-render()
+renderPage();
