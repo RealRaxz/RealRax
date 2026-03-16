5909d576f6b68dece58d354b2acd9bf65d549481
RealRaxz <xxzzoo500@gmail.com>
2026-03-16 10:29:30 +0700

    Create redirect.js

---
+++ redirect.js
@@ -0,0 +1,16 @@
+function openPage(id){
+
+// เปิด Smartlink
+window.open(
+"https://airconditionstrodefist.com/xexd2an06?key=8b286cd67ff1ac791222566d220cda9d",
+"_blank"
+)
+
+// ไปหน้าสคริปต์
+setTimeout(function(){
+
+window.location.href="script1.html?id="+id
+
+},400)
+
+}
