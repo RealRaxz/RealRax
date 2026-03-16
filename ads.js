f53b42572d0ee1727b3174343808d5df94f9ad27
RealRaxz <xxzzoo500@gmail.com>
2026-03-16 09:53:05 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,22 +1,25 @@
-/* POPUNDER */
+function loadAds(){
 
-(function(){
+  // Popunder
+  var pop = document.createElement("script");
+  pop.src = "https://pl28900655.effectivegatecpm.com/48/6b/e9/486be951557a940606cb3f3d6ce342d3.js";
+  pop.async = true;
+  document.body.appendChild(pop);
 
-var pop = document.createElement("script");
-pop.src = "https://pl28900655.effectivegatecpm.com/48/6b/e9/486be951557a940606cb3f3d6ce342d3.js";
-pop.async = true;
-document.body.appendChild(pop);
+  // Social Bar
+  var social = document.createElement("script");
+  social.src = "https://pl28905385.effectivegatecpm.com/8b/f9/c2/8bf9c2a00d1d723faf2111b54b17a1bd.js";
+  social.async = true;
+  document.body.appendChild(social);
 
-})();
+}
 
+// โหลดทุกครั้งที่เข้าเว็บหรือรีเฟรช
+window.addEventListener("load", loadAds);
 
-/* SOCIAL BAR */
-
-(function(){
-
-var social = document.createElement("script");
-social.src = "https://pl28905385.effectivegatecpm.com/8b/f9/c2/8bf9c2a00d1d723faf2111b54b17a1bd.js";
-social.async = true;
-document.body.appendChild(social);
-
-})();
+// ถ้าผู้ใช้สลับแท็บแล้วกลับมา
+document.addEventListener("visibilitychange", function(){
+  if(!document.hidden){
+    loadAds();
+  }
+});
