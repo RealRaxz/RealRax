21f1a1f814456264dc3cf1eb11a8ee2985d065e7
RealRaxz <xxzzoo500@gmail.com>
2026-03-16 08:43:02 +0700

    Update and rename script.js to ads.js

---
+++ ads.js
@@ -0,0 +1,21 @@
+/* POPUNDER */
+
+(function(){
+
+var s=document.createElement("script")
+s.src="https://pl28900655.effectivegatecpm.com/48/6b/e9/486be951557a940606cb3f3d6ce342d3.js"
+s.async=true
+document.body.appendChild(s)
+
+})();
+
+/* SOCIAL BAR */
+
+(function(){
+
+var s=document.createElement("script")
+s.src="https://pl28905385.effectivegatecpm.com/8b/f9/c2/8bf9c2a00d1d723faf2111b54b17a1bd.js"
+s.async=true
+document.body.appendChild(s)
+
+})();
