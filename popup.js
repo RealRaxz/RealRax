45e23a443494be04a3ce22738b7cf839e2cee5f1
RealRaxz <xxzzoo500@gmail.com>
2026-03-19 21:07:11 +0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", () => {
         <div id="pfYTStatus1" class="pf-status">เพื่อปลดล็อกขั้นต่อไป!</div>
 
         <button id="pfYT2" class="pf-btn pf-red pf-disabled">กดไลก์ คอมเมนต์ / 𝗟𝗶𝗸𝗲 & 𝗖𝗼𝗺𝗺𝗲𝗻𝘁</button>
-        <div id="pfYTStatus2" class="pf-status">ล็อกอยู่</div>
+        <div id="pfYTStatus2" class="pf-status">ล็อกอยู่!</div>
 
         <div id="pfProgress" class="pf-progress">
           <div class="pf-barBox"><div id="pfBar" class="pf-bar"></div></div>
