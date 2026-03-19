fa951abc738eb01213c167027306a7e5d4a09024
RealRaxz <xxzzoo500@gmail.com>
2026-03-19 20:08:27 +0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -63,10 +63,10 @@ document.addEventListener("DOMContentLoaded", () => {
       <div class="pf-panel">
         <div style="margin-bottom:10px;color:white;">ทำตามขั้นตอน / 𝗖𝗼𝗺𝗽𝗹𝗲𝘁𝗲 𝗦𝘁𝗲𝗽𝘀</div>
 
-        <button id="pfYT1" class="pf-btn pf-red">กดไลก์ & คอมเมนต์ / Like & Comment 1</button>
+        <button id="pfYT1" class="pf-btn pf-red">กดไลก์ & คอมเมนต์ / Like & Comment</button>
         <div id="pfYTStatus1" class="pf-status">กดไลก์และคอมเมนต์</div>
 
-        <button id="pfYT2" class="pf-btn pf-red pf-disabled">กดไลก์ & คอมเมนต์ / Like & Comment 2</button>
+        <button id="pfYT2" class="pf-btn pf-red pf-disabled">กดไลก์ & คอมเมนต์ / Like & Comment</button>
         <div id="pfYTStatus2" class="pf-status">ล็อคอยู่</div>
 
         <div id="pfProgress" class="pf-progress">
